0b4d3590788659ac1cbb1dc976b8eec9441db5e6
FABIO CHAVEZ s303519 <fabio.alfredo.fc.fc@gmail.com>
2024-02-19 17:29:24 +0100

    added frame fixer an test to labelCreation2.py

--- FrameFixer.py
+++ FrameFixer.py
@@ -1,14 +1,12 @@
-import os
 import numpy as np
-from keras.utils import to_categorical
-from sklearn.model_selection import train_test_split
+import os
 
 
 def get_sequences(directoryPath=''):
     get_sequences_var = os.listdir(directoryPath)
     j = []
     for i in range(len(get_sequences_var)):
-        if os.path.isdir(os.path.join(directoryPath,get_sequences_var[i])):
+        if os.path.isdir(os.path.join(directoryPath, get_sequences_var[i])):
             j.append(get_sequences_var[i])
 
     return j
@@ -19,7 +17,7 @@ if __name__ == '__main__':
     working_sequences, labels = [], []
     DATA_PATH = os.path.join('MP_data')
     list_actions = os.listdir(DATA_PATH)
-
+    zerosArray = np.zeros((21, 3))
     for action in list_actions:
         print(action)
         dirPath = os.path.join(DATA_PATH, action, )
@@ -36,13 +34,8 @@ if __name__ == '__main__':
                 # print(file)
                 if os.path.isfile(file):
                     res = np.load(file)
-                    window.append(res)
-            # print(len(window))
-            if len(window) > 0:
-                working_sequences.append(window)
-                labels.append(label_map[action])
+                    if len(res) != 21:
+                        np.save(file, zerosArray)
 
-    x = np.array(working_sequences)
-    y = to_categorical(labels).astype(int)
 
-    # X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.05)
+
