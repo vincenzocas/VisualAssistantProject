00cec0197a9ccd401776b3d4853f6ce37e4ecd1f
FABIO CHAVEZ s303519 <fabio.alfredo.fc.fc@gmail.com>
2024-02-19 18:06:21 +0100

    Update NNTraining.py
    now id doesn't need DataCollection

--- NNTraining.py
+++ NNTraining.py
@@ -2,21 +2,23 @@ from keras.models import Sequential
 from keras.layers import LSTM, Dense
 from keras.callbacks import TensorBoard
 import os
-import DataCollection as dc
 import LabelCreation2 as lb
+import numpy as np
+
+actions = np.array(['Minimize', 'Next', 'Previous', 'ScrollDown', 'ScrollUp', 'Volume'])
 
 log_dir = os.path.join('Logs')
 tb_callback = TensorBoard(log_dir=log_dir)
 
 model = Sequential()
-# 30 is the frame's number, 63 is the number of values, 3 for each hand landmarks (x, y , z)
+# 30 is the frame's number, 63 is the number of values, 3 for each hand landmark 21 * (x, y , z)
 model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30, 63)))
 model.add(LSTM(128, return_sequences=True, activation='relu'))
 model.add(LSTM(64, return_sequences=False, activation='relu'))
 model.add(Dense(64, activation='relu'))
 model.add(Dense(32, activator='relu'))
 # the last layer gives us an array of probability whose sum is 1
-model.add(Dense(dc.actions.shape[0], activation='softmax'))
+model.add(Dense(actions.shape[0], activation='softmax'))
 
 # Model Compile
 model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
