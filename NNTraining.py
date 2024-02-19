7fc55b9a1f6366e6ad5db7b38f7e3f9f9de98014
vincenzocas <vincenzo.casillo99@gmail.com>
2024-02-19 17:45:26 +0100

    fix line

--- NNTraining.py
+++ NNTraining.py
@@ -13,7 +13,7 @@ model = Sequential()
 model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30, 63)))
 model.add(LSTM(128, return_sequences=True, activation='relu'))
 model.add(LSTM(64, return_sequences=False, activation='relu'))
-model.add(Dense(64, activation='relu '))
+model.add(Dense(64, activation='relu'))
 model.add(Dense(32, activator= 'relu'))
 #the last layer gives us an array of probability whose sum is 1
 model.add(Dense(dc.actions.shape[0], activation='softmax'))
